6f6d5401a561212acd25b534fa2f6d8fb46c2f7f
Ephrem Yemru <ephrem.b.yemru@accenture.com>
2025-05-27 21:26:31 -0500

    Fix: use group block for Okta app group assignments

--- okta_app_group_assignments.tf
+++ okta_app_group_assignments.tf
@@ -1,4 +1,7 @@
 resource "okta_app_group_assignments" "demo_saml_app_group" {
-  app_id   = okta_app_saml.demo.id
-  group    = okta_group.ci_cd_test.id
+  app_id = okta_app_saml.demo.id
+
+  group {
+    id = okta_group.ci_cd_test.id
+  }
 }
