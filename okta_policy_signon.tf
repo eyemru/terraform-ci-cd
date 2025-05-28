08c30bea251742d94d6fa94783b3a27453cbaadb
Ephrem Yemru <ephrem.b.yemru@accenture.com>
2025-05-27 21:34:35 -0500

    Remove all Okta user creation and references from Terraform config

--- okta_policy_signon.tf
+++ okta_policy_signon.tf
@@ -1,7 +1,7 @@
 resource "okta_policy_signon" "demo" {
   name        = "Demo Sign-On Policy"
   status      = "ACTIVE"
-  description = "Sign-on policy for demo users."
+  description = "Sign-on policy for test group."
   priority    = 1
   groups_included = [okta_group.ci_cd_test.id]
 }
